a2658295eac371dace55970bc552986b744f245d
ranran53 <hinaran53@gmail.com>
2026-05-08 20:37:10 +0900

    Use full design reference as LP

--- script.js
+++
@@ -1,6 +0,0 @@
-const CTA_LINK = "https://note.com/";
-
-document.querySelectorAll("[data-cta-link]").forEach((link) => {
-  link.href = CTA_LINK;
-  link.addEventListener("click", () => link.blur());
-});
